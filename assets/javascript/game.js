f2a880074d6b28adf8fb25ecda71f55aec313df5
Townsend2014 <saymay.townsend@gmail.com>
2017-11-10 23:51:34 -0700

    Updatd Code

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l",
+    "m","n","o","p","q","r","s","t","u","v","w","x","y","z'"];
+
+// Scoreboard
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessesSoFar = [];
+
+// Computer random guessing
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+// Event Listeners
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+
+// Scoring
+
+    if(userGuess === computerGuess){
+        wins++;
+        document.getElementById('wins').innerHTML = wins;
+    }else{
+        guesses--;
+        document.getElementById('guesses').innerHTML = guesses;
+    }
+    if(guesses === 0){
+        losses++;
+        guesses = 10;
+        guessesSoFar = [];
+        document.getElementById('losses').innerHTML = losses;
+    }  
+ 
+
+}  
+
+
+// How to push userGuess var guessesSoFar = userGuess.push(); or guessesSoFar.push(userGuess);
